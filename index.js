6bf70d633c236b1cc0f8b054516a0215b4e14f64
cjia2003 <cjia2003@gmail.com>
2023-10-31 23:22:43 -0500

    refactor

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
 
 // Create express app
-const manager = express();
+const index = express();
 const port = 3000;
 const path = require('path');
-manager.set('views', path.join(__dirname, 'views'));
+index.set('views', path.join(__dirname, 'views'));
 
 // Create pool
 const pool = new Pool({
@@ -25,9 +25,9 @@ process.on('SIGINT', function() {
     process.exit(0);
 });
 	 	 	 	
-manager.set("view engine", "ejs");
+index.set("view engine", "ejs");
 
-manager.get('/', (req, res) => {
+index.get('/', (req, res) => {
     inventory = []
     pool
         .query('SELECT * FROM inventory;')
@@ -54,7 +54,7 @@ manager.get('/', (req, res) => {
 //         });
 // });
 
-manager.listen(port, () => {
+index.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
